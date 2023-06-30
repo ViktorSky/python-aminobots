1a6f7dc9c079534d42428edfbf860e7fba084408
Viktor <99266266+ViktorSky@users.noreply.github.com>
2023-06-30 17:56:53 -0400

    Add files via upload

---
+++ aminobots/models/linkresolution.py
@@ -0,0 +1,43 @@
+"""MIT License
+
+Copyright (c) 2022 ViktorSky
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from typing import Optional, Union
+from pydantic import Field
+from .model import Model
+from ..enums import ObjectType
+from ..objects.linkinfov2 import LinkInfoV2, Extensions
+
+__all__ = ('LinkResolution',)
+
+
+class LinkResolution(Model):
+    comId: int = property(lambda self: self.linkInfoV2.linkInfo.comId or self.linkInfoV2.community.id)
+    extensions: Extensions = property(lambda self: self.linkInfoV2.extensions)
+    linkInfoV2: LinkInfoV2 = Field(default_factory=LinkInfoV2)
+    objectId: Union[str, int] = property(lambda self: self.linkInfoV2.extensions.linkInfo.objectId)
+    objectType: ObjectType = property(lambda self: self.linkInfoV2.linkInfo.objectType)
+    path: str = property(lambda self: self.linkInfoV2.path, doc='NDC path')
+    fullPath: Optional[str] = property(lambda self: self.linkInfoV2.linkInfo.fullPath)
+    shareURLFullPath: Optional[str] = property(lambda self: self.linkInfoV2.linkInfo.shareURLFullPath)
+    shareURLShortCode: Optional[str] = property(lambda self: self.linkInfoV2.linkInfo.shareURLShortCode)
+    shortCode: Optional[str] = property(lambda self: self.linkInfoV2.linkInfo.shortCode)
+    targetCode: Optional[int] = property(lambda self: self.linkInfoV2.linkInfo.targetCode)
